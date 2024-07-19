ca8a8d9b7dc226e40694d7a3520fad982eb62d87
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2024-07-19 07:51:10 +0200

    correction of CharacterSteps.java file

--- Mp_Final/srcTest/CharacterSteps.java
+++ Mp_Final/srcTest/CharacterSteps.java
@@ -6,8 +6,8 @@ public class CharacterSteps {
     @Given("an user who wants to sign up in the app")
     public void an_user_who_wants_to_sign_up_in_the_app() {
     }
-    @When("he is sign-in up in the app and creating a character")
-    public void he_is_sign_in_up_in_the_app_and_creating_a_character() {
+    @When("he has signed up in the app and created a character")
+    public void he_has_signed_up_in_the_app_and_created_a_character() {
     }
     @Then("the user can defines the name of the character")
     public void the_user_can_defines_the_name_of_the_character() {
--- out/production/Calidad-Software-G1/resources/character.feature
+++ out/production/Calidad-Software-G1/resources/character.feature
@@ -2,5 +2,5 @@ Feature: User is able to add a name for its character
 
   Scenario: add a name for a character using a spy object
     Given an user who wants to sign up in the app
-    When he is sign-in up in the app and creating a character
+    When he has signed up in the app and created a character
     Then the user can defines the name of the character
